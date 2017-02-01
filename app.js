584b3c75c22547817a5b916fa2070ab4f27dc820
Vijayganesh7Sankaran <vgblazer@gmail.com>
2017-02-01 22:54:32 +0530

    updated

--- app.js
+++ app.js
@@ -3,93 +3,68 @@ var bodyParser = require('body-parser');
 var fs = require("fs");
 var cors = require('cors');
 
-
 var app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
-app.use(function(req,res,next){
-
-	console.log(req.method+" "+req.url);
+app.use(function (req, res, next) {
+	console.log(req.method + " " + req.url);
 	next();
 });
 
 app.use(express.static('./public/'));
 
-app.post('/postlogs', function(req, res) {
-  console.log(req.body.user);
-  //console.log(req.body.description);
-
-
-  		fs.readFile('./public/store.json', 'utf8', function(err,data){
-			if(err){
+app.post('/postlogs', function (req, res) {
+	console.log(req.body.user);
+	//console.log(req.body.description);
+	fs.readFile('./public/store.json', 'utf8', function (err, data) {
+		if (err) {
 			console.log(err);
-			}
-			else{
+		} else {
 			var jsondat = JSON.parse(data);
 			jsondat.user.push(req.body.user);
 			console.log(jsondat);
 
-			fs.writeFile('./public/store.json',JSON.stringify(jsondat),function(error){
-				if(error){
+			fs.writeFile('./public/store.json', JSON.stringify(jsondat), function (error) {
+				if (error) {
 					console.log(err);
-						}
-				else{
-
+				} else {
 					console.log("Write successful");
-  					res.send("Write Successful");
-					
-					}
-
-				});
-		}
-		
-			
+					res.send("Write Successful");
+				}
 			});
-  		
-  		//}
-
-  });
-  
-
-
-
-app.get('/getlogs',function(req, res){
-	console.log(req.query.ip);
-	var reqip=req.query.ip;
-
-	fs.readFile('./public/store.json', 'utf8', function(err,data){
-		if(err){
-			console.log(err);
 		}
-		else{
+	});
+});
+
+app.get('/getlogs', function (req, res) {
+	console.log(req.query.ip);
+	var reqip = req.query.ip;
+
+	fs.readFile('./public/store.json', 'utf8', function (err, data) {
+		if (err) {
+			console.log(err);
+		} else {
 			var jsondat = JSON.parse(data);
-			var stack =[];
-			var payload = {"user":[]};
-			var i=0;
-			for(i=0;i<jsondat.user.length;i++){
-				if(jsondat.user[i].ip === reqip){
+			var stack = [];
+			var payload = {
+				"user": []
+			};
+			var i = 0;
+			for (i = 0; i < jsondat.user.length; i++) {
+				if (jsondat.user[i].ip === reqip) {
 					payload.user.push(jsondat.user[i]);
 				}
-
-
 			}
-
 			console.log(JSON.stringify(payload));
-
-
-
 			res.send(payload);
-			}
-		
-			
+		}
 	});
-
-	
 });
 
 app.listen(3000);
 
 console.log("Server started...listening on port:3000");
-
